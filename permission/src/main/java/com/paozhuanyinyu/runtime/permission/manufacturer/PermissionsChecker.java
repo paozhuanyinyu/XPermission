20e10e0c53233ce870fd85f8afc988e6b461b956
喻凯 <kaiyu@caixin.com>
2020-03-13 01:48:31 +0800

    1.1.8

--- permission/src/main/java/com/paozhuanyinyu/runtime/permission/manufacturer/PermissionsChecker.java
+++ permission/src/main/java/com/paozhuanyinyu/runtime/permission/manufacturer/PermissionsChecker.java
@@ -71,7 +71,7 @@ public class PermissionsChecker {
                 case Manifest.permission.READ_CALL_LOG:
                     return checkReadCallLog(activity);
                 case Manifest.permission.READ_PHONE_STATE:
-                    boolean havePermission = isPermissionGranted(activity,permission);
+                    boolean havePermission = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && isPermissionGranted(activity,permission);
                     return havePermission;
                 case Manifest.permission.CALL_PHONE:
                     return defaultValue;
